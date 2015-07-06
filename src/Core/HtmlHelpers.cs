c929584fd7f002bb26b75c87e640e6eae9b03be7
Ken Dale <ken@kendaleiv.com>
2015-07-06 13:54:33 -0400

    Move functionality into Core project

---
+++ src/Core/HtmlHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace AspNetBrowserLocale.Core
+{
+    public static class HtmlHelpers
+    {
+        public static MvcHtmlString LocaleDateTime(this HtmlHelper htmlHelper, DateTime? dateTime)
+        {
+            if (dateTime.HasValue)
+            {
+                var elementId = Guid.NewGuid();
+                var msSinceUnixEpoch = (long)((TimeSpan)(dateTime.Value.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalMilliseconds;
+
+                return MvcHtmlString.Create(string.Format(
+@"<span id=""aspnet-browser-locale-{0}""></span>
+<script>
+    var jsDate = new Date({1});
+    document.getElementById('aspnet-browser-locale-{0}').innerHTML = jsDate.toLocaleString();
+</script>",
+elementId,
+msSinceUnixEpoch));
+            }
+            else
+            {
+                return MvcHtmlString.Create("&mdash;");
+            }
+        }
+    }
+}
