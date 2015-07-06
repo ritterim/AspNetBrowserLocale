d59ad7b443d8cc158b66ef4d3df0428f5b7693b5
Ken Dale <ken@kendaleiv.com>
2015-07-06 16:25:17 -0400

    Create InitializeLocaleDateTime to wire up all existing elements

--- src/Core/HtmlHelpers.cs
+++ src/Core/HtmlHelpers.cs
@@ -5,26 +5,36 @@ namespace AspNetBrowserLocale.Core
 {
     public static class HtmlHelpers
     {
+        public static MvcHtmlString InitializeLocaleDateTime(this HtmlHelper htmlHelper)
+        {
+            return MvcHtmlString.Create(
+@"<script>
+    var elements = document.querySelectorAll('[data-aspnet-browser-locale]');
+    for (var i = 0; i < elements.length; i++) {{
+        var element = elements[i];
+        var msString = element.dataset.aspnetBrowserLocale;
+        if (msString) {{
+            var jsDate = new Date(parseInt(msString));
+            element.innerHTML = jsDate.toLocaleString();
+        }}
+        else {{
+            element.innerHTML = '&mdash;';
+        }}
+    }}
+</script>");
+        }
+
         public static MvcHtmlString LocaleDateTime(this HtmlHelper htmlHelper, DateTime? dateTime)
         {
+            long? msSinceUnixEpoch = null;
             if (dateTime.HasValue)
             {
-                var elementId = Guid.NewGuid();
-                var msSinceUnixEpoch = (long)((TimeSpan)(dateTime.Value.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalMilliseconds;
+                msSinceUnixEpoch = (long)((TimeSpan)(dateTime.Value.ToUniversalTime() - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalMilliseconds;
+            }
 
-                return MvcHtmlString.Create(string.Format(
-@"<span id=""aspnet-browser-locale-{0}""></span>
-<script>
-    var jsDate = new Date({1});
-    document.getElementById('aspnet-browser-locale-{0}').innerHTML = jsDate.toLocaleString();
-</script>",
-elementId,
-msSinceUnixEpoch));
-            }
-            else
-            {
-                return MvcHtmlString.Create("&mdash;");
-            }
+            return MvcHtmlString.Create(string.Format(
+                @"<span data-aspnet-browser-locale=""{0}""></span>",
+                msSinceUnixEpoch));
         }
     }
 }
