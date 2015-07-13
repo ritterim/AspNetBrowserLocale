78614009a4267ae25ad8d781c029ebc63ef5c3ec
Ken Dale <ken@kendaleiv.com>
2015-07-13 11:58:40 -0400

    Setup solution info

---
+++ src/common/SolutionInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("ASP.NET Browser Locale")]
+[assembly: AssemblyCopyright("Copyright © 2015 Ritter Insurance Marketing")]
+[assembly: AssemblyCompany("Ritter Insurance Marketing")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
